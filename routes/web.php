46ea20ae533711080115cad76d33a0ad873294d7
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-20 10:07:19 +0100

    Routing for the welcome and blog.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\IntroPage;
+use \App\Http\controllers\BlogPage;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ---- Custom routing ----
+Route::get("/welcome_page",[IntroPage::class,"index"]) -> name("WelcomePage.index");
+Route::get("/blog", [BlogPage::class, "index"]) -> name("Blog.index");
+// -----------------------
