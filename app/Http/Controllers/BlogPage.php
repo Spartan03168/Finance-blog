4bd57a7f425d143a81d49fbc689f9a65811cc6b3
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-21 12:31:30 +0100

    Blog controller linked to model database access. Seems functional, currently untested.

--- app/Http/Controllers/BlogPage.php
+++ app/Http/Controllers/BlogPage.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Banks;
+use App\Models\Blogs_stored;
 
 class BlogPage extends Controller{
     public function index(){
         $db_access = 0;
-        $clearance_authorization = 0;
         if ($db_access === 0) {
             // Initial for now until I get the Banks and blogs model integrated
             return view("Blog.index");
         } elseif ($db_access === 1) {
             // Assuming the database has been properly set up
-            $clearance_authorization = 1;
+            $bank_data = Banks::banksDataExtraction();
+            $posts_data = Blogs_stored::postExtractionProtocol();
+            // Return process
+            return view('Blog.index', compact('bank_data', 'posts_data'));
         } else{
             // Failsafe response
             return view("Blog.index");
