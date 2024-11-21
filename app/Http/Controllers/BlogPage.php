f7133b1cfd3b93e764966d9e2283738612b13546
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-21 11:53:01 +0100

    Blog controller switch prepared for database integration.

--- app/Http/Controllers/BlogPage.php
+++ app/Http/Controllers/BlogPage.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Banks;
 
 class BlogPage extends Controller{
     public function index(){
-        return view("Blog.index");
+        $banks_access = 0;
+        $clearance_authorization = 0;
+        if ($banks_access === 0) {
+            return view("Blog.index");
+        } elseif ($banks_access === 1) {
+            $clearance_authorization = 1;
         }
+
     }
+}
