932b7c103469acb8157c116f021a284c203c6008
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-24 11:49:53 +0100

    Controller update to sync with the modified synced models.

--- app/Http/Controllers/BlogPage.php
+++ app/Http/Controllers/BlogPage.php
@@ -8,14 +8,15 @@ use App\Models\Blogs_stored;
 
 class BlogPage extends Controller{
     public function index(){
-        $db_access = 0;
+        $db_access = 1;
         if ($db_access === 0) {
             // Initial for now until I get the Banks and blogs model integrated
             return view("Blog.index");
         } elseif ($db_access === 1) {
             // Assuming the database has been properly set up
-            $bank_data = Banks::banksDataExtraction();
-            $posts_data = Blogs_stored::postExtractionProtocol();
+            // Fetching bank data
+            $bank_data = Banks::all();
+            $posts_data = Blogs_stored::orderBy('date_and_time_of_upload', 'desc')->get();
             // Return process
             return view('Blog.index', compact('bank_data', 'posts_data'));
         } else{
