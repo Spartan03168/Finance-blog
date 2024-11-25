0a683719eff981a1696285b88f5cdc1599826de5
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-25 08:26:50 +0100

    Authentication and middleware routing updates.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\IntroPage;
 use \App\Http\controllers\BlogPage;
@@ -8,9 +9,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// ---- Dashboard ----
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
 // ---- Custom routing ----
 Route::get("/welcome_page",[IntroPage::class,"index"]) -> name("WelcomePage.index");
 Route::get("/blog", [BlogPage::class, "index"]) -> name("Blog.index");
 // -----------------------
+
+require __DIR__.'/auth.php';
