92f5dd16545830d520dc24d1efe6043cf6fdda15
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-28 10:28:44 +0100

    Baseline for the post editing page complete.

--- resources/views/PostEditor.blade.php
+++ resources/views/PostEditor.blade.php
@@ -1,6 +1,18 @@
 <div>
     <!-- Styling section -->
-
+    <style>
+        /* Background logic */
+        div {
+            margin: 0;
+            padding: 0;
+            background-image: url("{{ asset("black-geometric-shape-background-design-free-vector.jpg") }}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+            position: relative;
+        }
+    </style>
     <!-- Header -->
     <header>Post editor mode</header>
     <!-- CRUD deployment -->
