ccc74506b58e07a3abc68945252a0b531958b8a0
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-28 10:01:43 +0100

    Post editor controller initial prototype.

--- app/Http/Controllers/PostEditor.php
+++ app/Http/Controllers/PostEditor.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Blogs_stored;
 use Illuminate\Http\Request;
 
-class PostEditor extends Controller
-{
-    //
-}
+class PostEditor extends Controller {
+    // Posts controller with access to posts database.
+    public function index() {
+        $posts_data = Blogs_stored::all();
+        return view('PostEditor', ['posts_data' => $posts_data]);
+        }
+    }
