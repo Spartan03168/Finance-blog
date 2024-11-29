6f87adf38a7785ae309c7fa122ea799b1a070d97
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-29 19:18:30 +0100

    Time span to keep the cache cut in half as well.

--- app/Http/Controllers/BlogPage.php
+++ app/Http/Controllers/BlogPage.php
@@ -22,12 +22,12 @@ class BlogPage extends Controller{
                 $posts_data = Blogs_stored::orderBy('date_and_time_of_upload', 'desc')->get();
             } elseif ($cache_mode === 1) {
                 // Posts data caching
-                $posts_data = Cache::remember("posts_data",now()->addMinutes(10), function (){
+                $posts_data = Cache::remember("posts_data",now()->addMinutes(5), function (){
                     return Blogs_stored::orderby('date_and_time_of_upload', 'desc')->get();
                     });
                 // ------------------
                 // Bank data caching
-                $bank_data = Cache::remember("bank_data", now()->addMinutes(10), function (){
+                $bank_data = Cache::remember("bank_data", now()->addMinutes(5), function (){
                     return Banks::all();
                     });
                 }
