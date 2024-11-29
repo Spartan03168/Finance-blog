7d7069c1cec4edf39b6ec15990a87183eaba3d77
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-29 18:10:42 +0100

    User admin access update.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticate;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticate
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        "is_admin"
     ];
 
     /**
