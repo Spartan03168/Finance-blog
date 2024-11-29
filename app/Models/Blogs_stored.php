60b242155a6aef44ef0740b5098c6efbcacc9517
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-30 00:20:19 +0100

    This was THE nightmare.

--- app/Models/Blogs_stored.php
+++ app/Models/Blogs_stored.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blogs_stored extends Model{
     // Columns: Post title, Date and time of upload, Content
-    protected $fillable = ["post_title", "date_and_time_of_upload", "content"];
+    protected $fillable = ["post_title", "date_and_time_of_upload", "content", "bank_id"];
     protected $table = "blogs";
     protected $casts = [
         "date_and_time_of_upload" => "datetime",
@@ -13,6 +13,6 @@ class Blogs_stored extends Model{
 
     // Relationship connection to blogs
     public function bank(){
-        return $this->belongsTo(Blogs_stored::class, "bank_id", "ban_name");
+        return $this->belongsTo(Blogs_stored::class, "bank_id", "id");
         }
     }
