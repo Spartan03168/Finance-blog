a049df832198fe7d85c75d50d058c70445b405f8
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-29 22:26:52 +0100

    Migration file also synced with new column.

--- database/migrations/2024_11_24_091835_create_blogs_table.php
+++ database/migrations/2024_11_24_091835_create_blogs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->string('title');
             $table->text("content");
             $table->timestamp("date");
+            $table->string("bank_id");
             $table->timestamps();
             });
         }
