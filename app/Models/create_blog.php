1a1b61599958181630c9a5433db9bb25d9c124ee
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-30 05:04:18 +0100

    Rerouted.

--- app/Models/create_blog.php
+++ app/Models/create_blog.php
@@ -8,8 +8,8 @@ use App\Models\Blogs_stored;
 use Illuminate\Routing\Controller;
 
 class create_blog extends Controller {
-    public function index(Request $request) {
+    public function create(Request $request) {
         $banks = Banks::all();
-        return view('create_blog_post', compact('banks'));
+        return view('blog.create', compact('banks'));
         }
     }
