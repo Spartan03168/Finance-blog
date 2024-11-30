ecd194e0974e4c9cb6d990c11adc7c6df4ecc311
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-30 07:33:57 +0100

    The concepts are here. The routing is just messed up.

--- app/Http/Controllers/BlogPage.php
+++ app/Http/Controllers/BlogPage.php
@@ -11,47 +11,8 @@ class BlogPage extends Controller{
 
     public function index() {
         $posts = Blogs_stored::with('bank')->get();
-        return view('blogs.index', compact('posts'));
+        return view('blog.index', compact('posts'));
         }
 
-    public function create() {
-        $banks = Banks::all();
-        return view('blogs.create', compact('banks'));
-        }
 
-    public function update(Request $request, $id) {
-        $validatedData = $request->validate ([
-            'title' => 'required|max:255',
-            'content' => 'required',
-            ]);
-
-        $blog = Blogs_stored::findOrFail($id);
-        $blog->update($validatedData);
-
-        return redirect()->route('Blog.index')->with('success', 'Post updated successfully.');
-        }
-
-    public function store(Request $request) {
-        $validated = $request->validate ([
-            'title' => 'required|max:255',
-            'content' => 'required',
-            'bank_id' => 'required|exists:banks,id',
-            ]);
-
-        Blogs_stored::create($validated);
-        return redirect()->route('blogs.store')->with('success', 'Post created successfully.');
-        }
-
-    public function delete($id) {
-        $blog = Blogs_stored::findOrFail($id);
-        $blog->delete();
-
-        return redirect()->route('Blog.index')->with('success', 'Blog deleted successfully.');
-        }
-
-    public function edit($id) {
-        $post = Blogs_stored::findOrFail($id);
-        $banks = Banks::all();
-        return view('blogs.edit', compact('post', 'banks'));
-        }
     }
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     // Blog related routes
     Route::resource('blogs', BlogPage::class)->names([
         'index' => 'blog.index',
