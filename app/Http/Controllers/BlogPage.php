cebe43ebda91521e235eb36952ccf62e1d278f0f
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-30 07:46:39 +0100

    Caching update.

--- app/Http/Controllers/BlogPage.php
+++ app/Http/Controllers/BlogPage.php
@@ -13,6 +13,15 @@ class BlogPage extends Controller{
         $posts = Cache::remember('posts_with_banks', 60, function () {
             return Blogs_stored::with('bank')->get();
             });
+        // Cache flushing after every update
+        Cache::tags(['blogs', 'banks'])->flush();
         return view('blog.index', compact('posts'));
+        }
+
+    public function getCachedPosts(){
+        return Cache::remember('posts_with_banks', 60, function () {
+            return Blogs_stored::with('bank')->get();
+            });
+        Cache::tags(['blogs', 'banks'])->flush();
         }
     }
