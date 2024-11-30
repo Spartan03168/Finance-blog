fd19ae2b228af5fe3ab92a65a8394819a63e7068
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-30 08:08:05 +0100

    Login mail notification methodology. Untested.

---
+++ app/Mail/LoginUpdates.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use http\Env\Request;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class LoginUpdates extends Mailable {
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct() {
+        //
+        }
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // --- Authentication passed ---
+            Mail::to(Auth::user()->email)->send(new LoginNotification());
+
+            return redirect()->intended('dashboard');
+                }
+            }
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope {
+        return new Envelope (
+            subject: 'Login Updates',
+            );
+        }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content {
+        return new Content(
+            view: 'view.name',
+            );
+        }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array {
+        return [];
+        }
+    }
---
+++ resources/views/Login_updates_view.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Notification</title>
+</head>
+<body>
+<h1>Login Alert</h1>
+<p>This is to notify you that your account was accessed.</p>
+</body>
+</html>
