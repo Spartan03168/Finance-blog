5a1e702c250045dd11b171d2abd8e4b30295b8ac
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-30 01:31:37 +0100

    Variable switch.

--- database/migrations/2024_11_29_230707_add_user_roles.php
+++ database/migrations/2024_11_29_230707_add_user_roles.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('name');
+            $table->string('role')->default('user');
             });
         }
     /**
